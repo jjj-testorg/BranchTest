8311d6b56bf30f6afdece64aa9d339675f921f85
Jeremy J. Johnson <jeremyjohnson7@mail.weber.edu>
2017-03-02 00:55:13 -0700

    Added hello world programs

---
+++ Hello.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Hello {
+	public static void Main(string[] args) {
+		for (int x = 1; x <= 10; x++)
+			Console.WriteLine(x);
+	}
+}
+
---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+	public status void main(String[] args) {
+		for (String a : args)
+			System.out.println(a);
+	}
+}
